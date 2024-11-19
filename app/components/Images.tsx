f7c40305b9df617ae209cbd9267f13c94fd1340e
favydabs <favourdabit@gmail.com>
2024-11-19 22:30:57 +0100

    fr

--- app/components/Images.tsx
+++ app/components/Images.tsx
@@ -1,42 +1,16 @@
-// import counter from "@/public/assets/counter.png";
-// import quizApp from "@/public/assets/quizApp.png";
-// import snip1 from "@/public/assets/snip1.png";
-// import youtubeClone from "@/public/assets/youtube_clone.png";
-// import bankApp from "@/public/assets/bankApp.png";
-// import todo from "@/public/assets/todo.png";
-// import pacman from "@/public/assets/pacman.png";
-// import logo from "@/public/assets/logo.png";
-// import email from "@/public/assets/email.png";
-// import X from "@/public/assets/X.png";
-// import linkedin from "@/public/assets/linkedin.png";
+import counter from "@/public/assets/counter.png";
+import quizApp from "@/public/assets/quizApp.png";
+import snip1 from "@/public/assets/snip1.png";
+import youtubeClone from "@/public/assets/youtube_clone.png";
+import bankApp from "@/public/assets/bankApp.png";
+import todo from "@/public/assets/todo.png";
+import pacman from "@/public/assets/pacman.png";
+import logo from "@/public/assets/logo.png";
+import email from "@/public/assets/email.png";
+import X from "@/public/assets/X.png";
+import linkedin from "@/public/assets/linkedin.png";
 
-// // export the images above
-// export {
-//   counter,
-//   quizApp,
-//   snip1,
-//   youtubeClone,
-//   bankApp,
-//   todo,
-//   pacman,
-//   logo,
-//   email,
-//   X,
-//   linkedin,
-// };
-
-import counter from "/public/assets/counter.png";
-import quizApp from "/public/assets/quizApp.png";
-import snip1 from "/public/assets/snip1.png";
-import youtubeClone from "/public/assets/youtube_clone.png";
-import bankApp from "/public/assets/bankApp.png";
-import todo from "/public/assets/todo.png";
-import pacman from "/public/assets/pacman.png";
-import logo from "/public/assets/logo.png";
-import email from "/public/assets/email.png";
-import X from "/public/assets/X.png";
-import linkedin from "/public/assets/linkedin.png";
-
+// export the images above
 export {
   counter,
   quizApp,
@@ -48,5 +22,6 @@ export {
   logo,
   email,
   X,
-  linkedin
+  linkedin,
 };
+
