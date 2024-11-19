83967aff8e06a56dbd3dba2733dc183eac0c76bc
favydabs <favourdabit@gmail.com>
2024-11-19 12:15:17 +0100

    updated  home page

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Projects from "@/app/projects/page";
+import About from "./about/page";
+
+const Home = () => {
+  return (
+    <>
+      <Projects />
+      <About />
+    </>
+  );
+};
+export default Home;
