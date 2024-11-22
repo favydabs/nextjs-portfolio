be42a83b739eb0b3d1757bb60ba2e70200510f44
favydabs <favourdabit@gmail.com>
2024-11-22 09:35:59 +0100

    editing

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,28 +1,74 @@
-"use client";
-
 import Image from "next/image";
 import Link from "next/link";
 import { motion } from "framer-motion";
-import { useEffect, useState } from "react";
+import {
+  counter,
+  quizApp,
+  snip1,
+  youtubeClone,
+  bankApp,
+  todo,
+  pacman,
+} from "../components/Images";
+
+const projects = [
+  {
+    image: counter,
+    title: "Passenger-Counter-App",
+    description:
+      "This application is designed to streamline the process of tracking passenger numbers. It offers solutions associated with manual counting methods.",
+    demo: "https://boisterous-caramel-06d791.netlify.app",
+  },
+  {
+    image: quizApp,
+    title: "Quiz-App",
+    description:
+      "This application is designed to provide users with an interactive quiz experience. It presents users with a series of questions related to JavaScript programming.",
+    demo: "https://favydabs.github.io/Quiz-App/",
+  },
+  {
+    image: snip1,
+    title: "Ecommerce-Web-App",
+    description:
+      "The Ecommerce Web App is a fully-functional online platform designed to facilitate buying and selling of products or services over the internet.",
+    demo: "https://favydabs.github.io/Ecommerce-Web-App/",
+  },
+  {
+    image: youtubeClone,
+    title: "YouTube Clone",
+    description:
+      "This is a simplified version of YouTube, replicating some of its core functionalities. It serves as an educational tool for those interested in learning.",
+    demo: "https://deft-smakager-197191.netlify.app",
+  },
+  {
+    image: bankApp,
+    title: "Bank-Landing-Page",
+    description:
+      "The bank app is crafted to provide users with a seamless and secure banking experience. With a clean and intuitive interface.",
+    demo: "https://favydabs.github.io/Bank-landing-page/",
+  },
+  {
+    image: todo,
+    title: "To-Do-List-App",
+    description:
+      "To-Do List App helps you manage your daily activities. The app makes it easy to create, prioritize, and track your to-do items with just a few taps.",
+    demo: "https://favydabs.github.io/to-do-list-web2acx/",
+  },
+  {
+    image: pacman,
+    title: "Pacman (using arrow keys)",
+    description:
+      "In this game, the objective is to eat all the dots (pellets) while avoiding four ghosts.",
+    demo: "https://favydabs.github.io/Pac-man_Game/",
+  },
+];
 
 const Projects = () => {
-  const [projects, setProjects] = useState([]);
-
-  useEffect(() => {
-    const fetchProjects = async () => {
-      const response = await fetch("/data.json");
-      const data = await response.json();
-      setProjects(data);
-    };
-
-    fetchProjects();
-  }, []);
-
   return (
     <section className="py-16 px-8">
       <div>
         <p className="lg:text-2xl font-semibold lg:mb-10 mb-4">
-          {`These are the most recent projects I've worked on, however I've worked on many throughout the years. Please get in touch with me if something catches your attention.`}
+          { These are the most recent projects I've worked on, however I've worked on many throughout the years. Please get in touch with me if something catches your attention.}
         </p>
       </div>
       <h2 className="text-4xl font-bold text-yellow-500 mb-8">My Projects</h2>
@@ -65,4 +111,4 @@ const Projects = () => {
   );
 };
 
-export default Projects;
+export default Projects; 
